ea5ea8d64210fce82502e2f285611056cabd4e7f
Nickolans Griffith <nickolansgriffith@gmail.com>
2022-03-16 09:08:16 -0500

    Initial Commit

---
+++ healthkit-example/healthkit_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  healthkit_exampleApp.swift
+//  healthkit-example
+//
+//  Created by Nickolans Griffith on 3/16/22.
+//
+
+import SwiftUI
+
+@main
+struct healthkit_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
